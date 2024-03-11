e18cca1de255aecb8b90cd2ddaed0933620c39c6
Shyam792004 <shyammakesh79@gmail.com>
2024-03-11 12:05:52 +0530

    Update App.jsx

--- parikchitra/src/App.jsx
+++ parikchitra/src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
 
 function App() {
- 
+
   return (
     <>
-    <div className='choice-container'>
-      <ul className='choice'>
-        <li>Student</li>
-        <li>Teacher</li>
-      </ul>
-    </div>
+      <div className='choice-container'>
+        <ul className='choice'>
+          <li>Student</li>
+          <li>Teacher</li>
+          <li>Login</li>
+        </ul>
+      </div>
     </>
   )
 }
