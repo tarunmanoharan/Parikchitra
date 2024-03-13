fd68cb72d8145788f3867adfd36af83cf5a3fa21
tarunmanoharan <98203435+tarunmanoharan@users.noreply.github.com>
2024-03-13 18:02:14 +0530

    updated home page

--- parikchitra/src/App.jsx
+++ parikchitra/src/App.jsx
@@ -1,24 +1,39 @@
 import './App.css'
 import  Footer  from './components/Footer'
 
-import Bg from './components/Bg'
 
 function App() {
 
   return (
     <>
-      <Bg/>
-      <div className='choice-container'>
-         
+
+      <div className='main-container'>
+
+        <ul className="box">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+
         <div className='title'>
           <h1>Parikchitra</h1>
         </div>
-        
+
+        <div className='choice-container'>
+
         <ul className='choice'>
           <li>Student</li>
           <li>Teacher</li>
         </ul>
+
+        </div>
+
       </div>
+
+
 
       <Footer/>
     </>
