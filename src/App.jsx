7e5ab3ae15da1a95b87891c9fab020c75540044e
tarunmanoharan <98203435+tarunmanoharan@users.noreply.github.com>
2024-03-16 11:19:13 +0530

    updating the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,24 @@
 import './App.css'
-import  Footer  from './components/Footer'
-
 import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate,
   Link
 } from "react-router-dom"
 
+import  Footer  from './components/Footer'
 
 import Slogin from './assets/student/components/Slogin'
+import Choice from './components/Choice'
 
 function App() {
 
   return (
-    <Router>
-
-      <div className='main-container'>
-
-        <ul className="box">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-
-        <div className='title'>
-          <h1>Parikchitra</h1>
-        </div>
-
-        <div className='choice-container'>
-
-        <div className='choice'>
-          <Link to="/slogin">
-            <button>Student</button>
-            </Link>
-          <button>Teacher</button>
-        </div>
-
-        </div>
-
-      </div>
-
-      <Footer/>
-
-    <Routes>
-      <Route path="/slogin" element={<Slogin/>}/>
-    </Routes>
-
-    </Router>
+    <>
+     <Choice/>
+     <Footer/>
+    
+    </>
   )
 }
 
---
+++ src/components/Choice.jsx
@@ -0,0 +1,40 @@
+
+import React from 'react'
+import './Choice.css'
+
+function Choice() {
+  return (
+    <>
+    <div className='main-container'>
+
+<ul className="box">
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ul>
+
+<div className='title'>
+  <h1>Parikchitra</h1>
+</div>
+
+<div className='choice-container'>
+
+<div className='choice'>
+  <button>Student</button>
+
+  <button>Teacher</button>
+</div>
+
+</div>
+
+</div>
+    </>
+  )
+}
+
+export default Choice
+
+
