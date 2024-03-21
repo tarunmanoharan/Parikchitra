a52d7a7ffc05de8153ca0da145ca367317e88307
tarunmanoharan <98203435+tarunmanoharan@users.noreply.github.com>
2024-03-21 18:06:35 +0530

    done some footer alignment

--- src/assets/student/s_components/Scourses.jsx
+++ src/assets/student/s_components/Scourses.jsx
@@ -9,16 +9,17 @@ const Scourses = () => {
           <span>Explore Courses</span>
         </div>
         <div className="enrolled-courses">
-          <span>Enrolled</span>
+          <span>Enrolled Courses</span>
         </div>
       </div>
+      
       <div className="courses-content">
         <div className="explore-courses-list">
-          <h2>Explore Courses</h2>
+          
           {/* Add your course list components here */}
         </div>
         <div className="enrolled-courses-list">
-          <h2>Enrolled Courses</h2>
+          
           {/* Add your enrolled course list components here */}
         </div>
       </div>
